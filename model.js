ef9443c424bd5c7f3175b387f490dd288a041d07
tharunoptimus <tharunoptimus@outlook.com>
2022-11-01 03:10:57 +0530

    Added the co-ordinates after path tracing

---
+++ model.js
@@ -0,0 +1,91 @@
+
+let firstSpot = {
+    x: -2.80,
+    y: -1.72,
+    z: 0.58
+}
+
+let secondSpot = {
+    x: -3.40,
+    y: -1.90,
+    z: 10.14
+}
+
+let thirdSpot = {
+    x: -2.65,
+    y: -1.80,
+    z: 21.09
+}
+
+let fourthSpot = {
+    x: -12.31,
+    y: 1.05,
+    z: 28.45
+}
+
+let fifthSpot = {
+    x: -32.66,
+    y: -2.51,
+    z: 28.25
+}
+
+let sixthSpot = {
+    x: -8.83,
+    y: 2.11,
+    z: 21.32
+}
+
+let seventhSpot = {
+    x: -11.15,
+    y: 0.54,
+    z: 2.87
+}
+
+let eightSpot = {
+    x: -31.94,
+    y: -0.56,
+    z: 2.35
+}
+
+let ninethSpot = {
+    x: -31.08,
+    y: -1.64,
+    z: -24.85
+}
+
+let tenthSpot = {
+    x: -9.51,
+    y: 0.68,
+    z: -24.96
+}
+
+let eleventhSpot = {
+    x: 4.29,
+    y: 0.61,
+    z: -20.80
+}
+
+let twelvethSpot = {
+    x: 5.54,
+    y: 0.77,
+    z: -12.01
+}
+
+let thirteenthSpot = {
+    x: 3.22,
+    y: 0.66,
+    z: -15.41
+}
+
+let fourteenthSpot = {
+    x: 17.92,
+    y: -1.23,
+    z: 5.08
+}
+
+let lastSpot = {
+    x: -15.61,
+    y: 17.13,
+    z: 56.66
+}
+
