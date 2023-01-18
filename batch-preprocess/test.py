ab518060378909967edeee459ff6a5d3558c2dc8
Rozen Berg <rozenberg4christ@gmail.com>
2023-01-18 08:04:43 +0000

    Added tests

---
+++ batch-preprocess/test.py
@@ -0,0 +1,11 @@
+
+print("Loading model...")
+
+print("Reducing polycount...")
+initial_polycount = 1000000
+reduced_polycount = 750000
+print("Initial polycount:", initial_polycount)
+print("Reduced polycount:", reduced_polycount)
+print("Polycount reduced by", initial_polycount - reduced_polycount, "vertices")
+
+print("Model loaded and polycount reduced.")
